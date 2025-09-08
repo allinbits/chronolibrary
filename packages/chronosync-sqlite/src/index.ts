a772b20ddd52067af794f86eb4a4e0d9cf808ab1
stuyk <trevorwessel@protonmail.com>
2025-09-08 09:05:52 -0600

    fix: JSON stringify false flag

--- packages/chronosync-sqlite/src/index.ts
+++ packages/chronosync-sqlite/src/index.ts
@@ -10,15 +10,27 @@ const lastBlock = db.lastBlock.select.get(1) as { id: number; block_value: strin
 const state = new ChronoState({ ...config, START_BLOCK: lastBlock.block_value });
 
 function handleAction(action: Action) {
+    let messages: string | undefined;
+    
+    try {
+        messages = JSON.stringify(action.messages);
+    } catch(err) {
+        console.error(err)
+    }
+
+    if (typeof messages === 'undefined') {
+        console.warn(`Failed to parse messages to JSON for action ${action.hash}`)
+        return;
+    }
+
     try {
         //hash, height, timestamp, memo, messages
-
         db.action.insert.run(
             action.hash,
             action.height,
             action.timestamp,
             action.memo,
-            JSON.stringify(action.messages)
+            messages
         );
     } catch (err) {
         console.error(err);
